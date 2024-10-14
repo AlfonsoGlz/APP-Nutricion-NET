5884cf355eb3d78800c322fe68d4a9235596edbf
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-10-14 00:12:29 -0600

    Se agrega ViewModel

---
+++ APP_Nutricion/Models/ViewModels/UpsertVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace APP_Nutricion.Models.ViewModels
+{
+    public class UpsertVM
+    {
+        public Alimento ?AlimentoItems { get; set; }
+        public IEnumerable<SelectListItem> ?UnidadesList { get; set;}
+        public IEnumerable<SelectListItem> ?CategoriasList { get; set; }
+    }
+}
