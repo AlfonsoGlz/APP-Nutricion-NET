526158a96b00e9ee15e0618a5f6765b6a6ab6ec2
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-10-13 21:49:36 -0600

    Se agrega controlador alimentos

---
+++ APP_Nutricion/Controllers/AlimentosController.cs
@@ -0,0 +1,22 @@
+﻿using APP_Nutricion.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APP_Nutricion.Controllers
+{
+    public class AlimentosController : Controller
+    {
+        private readonly IAlimentosService _service;
+
+        public AlimentosController(IAlimentosService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var alimentos = _service.ListarAlimentos();
+            return View(alimentos);
+        }
+
+    }
+}
