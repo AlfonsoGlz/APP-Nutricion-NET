239e6bc30d95ab7663ef75fc1ea474f67ec49887
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-10-13 21:48:09 -0600

    Agrega servicios de Alimentos

---
+++ APP_Nutricion/Services/AlimentosService.cs
@@ -0,0 +1,64 @@
+﻿using APP_Nutricion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APP_Nutricion.Services
+{
+    public class AlimentosService : IAlimentosService
+    {
+        private readonly APP_NutricionContext _context;
+
+        public AlimentosService (APP_NutricionContext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Alimento> ListarAlimentos()
+        {
+            return _context.Alimentos.Include("Unidad").ToList();
+        }
+
+        public Alimento ObtenerAlimentoId(int id)
+        {
+            Alimento alimento = _context.Alimentos.First(a => a.IdAlimento == id);
+            return alimento;
+        }
+
+        public void AgregarAlimento(Alimento a)
+        {
+            _context.Alimentos.Add(a);
+            _context.SaveChanges();
+        }
+
+        public void EditarAlimento(Alimento a)
+        {
+            Alimento alimento = _context.Alimentos.First(a => a.IdAlimento == a.IdAlimento);
+
+            alimento.NombreAlimento = a.NombreAlimento;
+            alimento.Cantidad = a.Cantidad;
+            alimento.Unidad = a.Unidad;
+            alimento.Calorias = a.Calorias;
+            alimento.Proteinas = a.Proteinas;
+            alimento.Carbohidratos = a.Carbohidratos;
+            alimento.Grasas = a.Grasas;
+            alimento.Fibra = a.Fibra;
+            alimento.Sodio = a.Sodio;
+            alimento.Categoria = a.Categoria;
+
+            _context.SaveChanges();
+
+        }
+
+        public void EliminarAlimento(int id)
+        {
+            Alimento alimento = _context.Alimentos.First(a => a.IdAlimento == id);
+
+            _context.Alimentos.Remove(alimento);
+            _context.SaveChanges();
+        }
+
+
+
+
+    }
+}
---
+++ APP_Nutricion/Services/IAlimentosService.cs
@@ -0,0 +1,13 @@
+﻿using APP_Nutricion.Models;
+
+namespace APP_Nutricion.Services
+{
+    public interface IAlimentosService
+    {
+        List<Alimento> ListarAlimentos();
+        Alimento ObtenerAlimentoId(int id);
+        void AgregarAlimento(Alimento a);
+        void EditarAlimento(Alimento a);
+        void EliminarAlimento(int id);
+    }
+}
